9e59f240ce8da5bde2252899ba783b22048ad27c
AlexNovikova <alexadubinina@mail.ru>
2021-10-29 14:40:48 +0700

    незначит изменения

--- src/main/java/ru/geekbrains/spring/controllers/ProductController.java
+++ src/main/java/ru/geekbrains/spring/controllers/ProductController.java
@@ -35,7 +35,6 @@ public class ProductController {
                     "%"+partOfName+"%");
             model.addAttribute("page", page);
             return "index";
-
     }
 
 //    @GetMapping("/find/{category_id}")
